9809e34bbc8ce0efd9620fe0916ecaa67784a295
Programoworm <rbkumar712@gmail.com>
2021-03-08 17:57:31 +0530

    Neural Network using Tensorflow created

--- intro.py
+++ intro.py
@@ -13,13 +13,20 @@ class DNet:
 	def forward(self,i):
 		self.op=np.dot(i,self.w)
 
+#Softmax Activation
+class SMA:
+	def __init__(self,i):
+		self.vals=np.exp(i-np.max(i, axis=1, keepdims=True))
+		self.probs=self.vals/np.sum(self.vals, axis=1, keepdims=True)
+
 def main():
 	print(str(sys.version)+"\n"+str(mp.__version__)+"\n"+str(np.__version__)+"\n")
 	L1=DNet(4,5)
 	Final=DNet(5,2)
 	L1.forward(X)
 	Final.forward(L1.op)
-	print(Final.op)
+	F=SMA(Final.op)
+	print(F.probs)
 
 if __name__ == '__main__':
 	main()
