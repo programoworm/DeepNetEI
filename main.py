0abdd9855cbc8a1f57d7733d4b3450c5b523b49e
Programoworm <rbkumar712@gmail.com>
2021-03-08 22:14:25 +0530

    DeepNet constructor completed

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 class DeepNet:
-	def __init__(self):
-		pass
+	def __init__(self,shape_x,shape_y,X_train,Y_train):
+		self.model = keras.Sequential([keras.layers.Flatten(input_shape=(28, 28)), keras.layers.Dense(100, activation='relu'),keras.layers.Dense(10, activation='sigmoid')])
+		self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+		self.model.fit(X_train, Y_train, epochs=10)
+
 def main():
 	print('DeepNet using TensorFlow')
 
