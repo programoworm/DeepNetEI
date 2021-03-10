f7b90524060db00ba9bc825ca91390f4bd260a5e
Programoworm <rbkumar712@gmail.com>
2021-03-10 21:20:44 +0530

    Accuracy graph plotting added

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-
+import matplotlib.pyplot as plt
 class DeepNet:
-	def __init__(self, shape_x, shape_y, X_train, Y_train):
+	def __init__(self, shape_x, shape_y, X_train, Y_train, X_test, Y_test):
 		self.model = keras.Sequential([keras.layers.Flatten(input_shape=(shape_x,shape_y)), 
 			keras.layers.Dense(5, activation='relu'),
 			keras.layers.Dense(12, activation='relu'),
 			keras.layers.Dense(1, activation='sigmoid')])
 		self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-		self.model.fit(X_train, Y_train, epochs=250, batch_size=10)
+		self.h=self.model.fit(X_train, Y_train, epochs=250, batch_size=10, validation_data=(X_test,Y_test))
 	
 	def test(self,X_test,Y_test):
 		self.model.evaluate(X_test,Y_test)
@@ -25,15 +25,15 @@ def main():
 	X_train=data[:,0:8]
 	Y_train=data[:,8]
 
-	#Model creation and training by constructor
-	dlmod=DeepNet(8, 1, np.array(X_train), np.array(Y_train))
-	
 	#Testing data modelling
 	data=np.loadtxt("./Datasets/d_test.data",delimiter=",")
 	X_test=data[:,0:8]
 	Y_test=data[:,8]
 	print("\nTest Results\n============\n")
 	
+	#Model creation and training by constructor
+	dlmod=DeepNet(8, 1, np.array(X_train), np.array(Y_train),X_test,Y_test)
+		
 	#Testing data using test() method of the model
 	dlmod.test(np.array(X_test),np.array(Y_test))
 	
@@ -41,6 +41,13 @@ def main():
 	#the neuron produced value
 	y=np.round(dlmod.model.predict(X_test))
 	print(y[2])
-
+	e=np.arange(1,1251,5)
+	plt.plot(e,dlmod.h.history['accuracy'],'g',label='Training Accuracy')
+	plt.plot(e,dlmod.h.history['val_accuracy'],'b',label='Testing Accuracy')
+	plt.title('Accuracy plotting')
+	plt.xlabel('Epoch')
+	plt.ylabel('Accuracy')
+	plt.legend()
+	plt.show()
 if __name__=='__main__':
 	main()
