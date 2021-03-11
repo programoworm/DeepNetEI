b1e784392966f859f57a7576f75221f1694bc8cf
Programoworm <rbkumar712@gmail.com>
2021-03-11 10:41:10 +0530

    heart dataset generated from heart.csv to rand_heart.data

--- Datasets/h_d_mod.py
+++ Datasets/h_d_mod.py
@@ -2,8 +2,20 @@ import random
 def mod(f):
     s=[]
     for line in f:
-        s.append(line)
-    print(s)
+        s.append(line.split(","))
+    us=[]
+    for i in range(1,len(s)):
+        t=""
+        for j in range(len(s[i])-1):
+            t=t+s[i][j]+","
+        t=t+s[i][len(s[i])-1][:-1]
+        us.append(t)
+    fn=open("rand_heart.data","a")
+    c=list(range(len(us)))
+    random.shuffle(c)
+    print(us)
+    while c:
+        fn.write(us[c.pop()]+"\n")
 def main():
     f=open("heart.csv")
     mod(f)
