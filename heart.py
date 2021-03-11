e74af7b339edcd4a7f6545869bbe395653bda0b7
Programoworm <rbkumar712@gmail.com>
2021-03-11 18:23:47 +0530

    heart model  updated

--- heart.py
+++ heart.py
@@ -1,24 +1,38 @@
 # Heart Dataset 
-from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 import matplotlib.pyplot as plt
+
+np.random.seed(0)
+
 # load the dataset
-dataset = loadtxt('rand_heart.data', delimiter=',')
+dataset=np.loadtxt('./Datasets/rand_heart.data', delimiter=',')
+
 # split into input (X) and output (y) variables
-X = dataset[:,0:13]
-y = dataset[:,13]
+X_train=dataset[:250,0:13]
+y_train=dataset[:250,13]
+
+X_test=dataset[251:,0:13]
+y_test=dataset[251:,13]
 # define the keras model
-model = Sequential()
+'''model=keras.Sequential()
 model.add(Dense(13, input_dim=13, activation='relu'))
 model.add(Dense(6, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+'''
+model=keras.Sequential([keras.layers.Flatten(input_shape=(13,1)), 
+			keras.layers.Dense(5, activation='relu'),
+			keras.layers.Dense(12, activation='relu'),
+			keras.layers.Dense(1, activation='sigmoid')])
 # compile the keras model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 # fit the keras model on the dataset
-history = model.fit(X, y, validation_split=0.33, epochs=250, batch_size=10, verbose=0)
+history = model.fit(X_train, y_train, validation_split=0.33, epochs=250, batch_size=10)
 # evaluate the keras model
-_, accuracy = model.evaluate(X, y)
+_, accuracy = model.evaluate(X_test, y_test)
 print('Accuracy: %.2f' % (accuracy*100))
 # list all data in history
 print(history.history.keys())
