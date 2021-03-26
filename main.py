adcfd83dea0202dd14ce69f0b0f71af8db1b5350
KBasak5 <kbasak26may@gmail.com>
2021-03-27 00:44:24 +0530

    main modified

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from diabetes import diabetes
 from heart import heart
 
+
 def main():
-	ch=input('Type 1,2 or 3:\n1.Diabetes\n2.Heart\n3.Exit\n')
-	while ch!=3:
-		if ch=='1':
-			diabetes()
-		elif ch=='2':
-			heart()
-		elif ch=='3':
-			exit()
-		else:
-			print("Invalid input!!")
+	while True :
 		ch=input('Type 1,2 or 3:\n1.Diabetes\n2.Heart\n3.Exit\n')
+		if(ch=='3'): exit()
+		switcher={
+			1: diabetes,
+			2: heart
+		}
+	
+		f=switcher.get(int(ch),"Invalid Input!!")
+		f()
+			
+		
 
 if __name__=='__main__':
 	main()
