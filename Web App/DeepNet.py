abc825ca656ec04dae3dacf4637f7b6f33bbb202
programoworm <rbkumar712@gmail.com>
2021-06-11 11:00:18 +0530

    Covid Web Page added

--- Web App/DeepNet.py
+++ Web App/DeepNet.py
@@ -2,14 +2,15 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 from tensorflow import keras
-
+import matplotlib.pyplot as plt
+from datetime import datetime
 class DeepNet:
 	
-	def __init__(self, shape_x, shape_y, opt, l, neurons, mname):
+	def __init__(self, shape_x, shape_y, opt, l, neurons, mname, a):
 		self.model=keras.Sequential([keras.layers.Flatten(input_shape=(shape_x,shape_y))]) 
 		for n in neurons:
 			self.model.add(keras.layers.Dense(n, activation='relu'))
-		self.model.add(keras.layers.Dense(shape_y, activation='sigmoid'))
+		self.model.add(keras.layers.Dense(shape_y, activation=a))
 		self.model.compile(optimizer=opt, loss=l, metrics=['accuracy'])
 		self.mname=mname
 	
@@ -17,4 +18,23 @@ class DeepNet:
 		print('\nTraining the model\n=================\n')
 		self.h=self.model.fit(X_train, Y_train, epochs=e, batch_size=bs, validation_data=(X_test,Y_test))
 		print("\nTest Results for ",self.mname,"\n=========================\n")
+		s=datetime.now()
 		self.model.evaluate(X_test,Y_test)
+		self.t=datetime.now()-s
+
+	def plot(self,e):
+		plt.plot(e,self.h.history['accuracy'])
+		plt.plot(e,self.h.history['val_accuracy'])
+		plt.title('model accuracy')
+		plt.ylabel('accuracy')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc='upper left')
+		plt.show()
+		# summarize history for loss
+		plt.plot(e,self.h.history['loss'])
+		plt.plot(e,self.h.history['val_loss'])
+		plt.title('model loss')
+		plt.ylabel('loss')
+		plt.xlabel('epoch')
+		plt.legend(['train', 'test'], loc='upper left')
+		plt.show()
--- Web App/heart.py
+++ Web App/heart.py
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 import DeepNet as dn
 
 #Training data modelling
-data=np.loadtxt("./Datasets/rand_heart.data",delimiter=",",comments='#')
+data=np.loadtxt("Datasets/rand_heart.data",delimiter=",",comments='#')
 X = data[:,0:13]
 y = data[:,13]
 
@@ -44,6 +44,6 @@ dlmod.model.fit(X, y, epochs=250, batch_size=10)
 # evaluate the keras model
 _, accuracy = dlmod.model.evaluate(X, y)
 # save the model
-dlmod.model.save("model1.h5")
+dlmod.model.save("heart.h5")
 print("Saved model to disk")
 
